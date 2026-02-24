478fecccc741d7008e405ecae384801adfcbe507
Phasakorn <pchivatx@andrew.cmu.edu>
2026-02-24 05:10:00 +0000

    front-end UI changes to Anonymous posting for quick replies posts

--- plugins/ta-resolve/public/ta-resolve-composer.js
+++ plugins/ta-resolve/public/ta-resolve-composer.js
@@ -63,6 +63,22 @@
 				return replyData;
 			});
 
+			hooks.on('filter:composer.submit', function (submitData) {
+				if (!submitData || !submitData.composerEl || !submitData.composerData) {
+					return submitData;
+				}
+				if (submitData.action !== 'topics.post' && submitData.action !== 'posts.reply') {
+					return submitData;
+				}
+
+				const select = submitData.composerEl.find('[data-component="composer-anonymous-toggle"]');
+				if (select && select.length && select.val() === 'anonymous') {
+					submitData.composerData.isAnonymous = true;
+				}
+
+				return submitData;
+			});
+
 			hooks.on('action:ajaxify.end', function () {
 				if (typeof ajaxify !== 'undefined' && ajaxify.data && ajaxify.data.template && ajaxify.data.template.topic) {
 					injectQuickReplyDropdown();
@@ -74,27 +90,6 @@
 				if (data && data.container) {
 					injectFullComposerDropdown(data.container);
 				}
-			});
-
-			hooks.on('filter:api.options', function (payload) {
-				if (!payload || !payload.options) {
-					return payload;
-				}
-				const options = payload.options;
-				if (options.method !== 'POST' || !options.url || options.url.indexOf('/topics') === -1) {
-					return payload;
-				}
-				if (window.__taResolveComposerAnonymous && options.data) {
-					try {
-						const body = typeof options.data === 'string' ? JSON.parse(options.data) : options.data;
-						body.data = body.data || {};
-						body.data.isAnonymous = true;
-						options.data = typeof payload.options.data === 'string' ? JSON.stringify(body) : body;
-					} catch (e) {
-						// ignore
-					}
-				}
-				return payload;
 			});
 		});
 	}
