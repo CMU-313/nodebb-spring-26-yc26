a0302145ff18f2f30c2abae87a270febd56e0af3
Raul Melendez <raulmelendez088@gmail.com>
2026-03-26 05:07:29 +0000

    feat: wire translation UI to deployed flask microservice

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,14 +4,14 @@
 
 const translatorApi = module.exports;
 
-translatorApi.translate = function (postData) {
-	return ['is_english',postData];
-};
-
-// translatorApi.translate = async function (postData) {
-//  Edit the translator URL below
-//  const TRANSLATOR_API = "TODO"
-//  const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-//  const data = await response.json();
-//  return ['is_english','translated_content'];
+// translatorApi.translate = function (postData) {
+// return ['is_english',postData];
 // };
+
+translatorApi.translate = async function (postData) {
+//  Edit the translator URL below
+	const TRANSLATOR_API = `http://17313-team09.s3d.cmu.edu:5000`;
+	const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+	const data = await response.json();
+	return [data.is_english, data.translated_content];
+};
